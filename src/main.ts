6f60a2982322fedec08ff564020a00bd37efb58b
Doug Binder <douglasjbinder@gmail.com>
2022-02-17 16:47:09 -0600

    chore: return proof and public signals

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
-import LRUMap from "mnemonist/lru-cache";
 const snarkjs = require("snarkjs");
 
 import {
   SnarkJSProofAndSignals,
-  buildContractCallArgs,
   MoveSnarkInput,
-  MoveSnarkContractCallArgs,
   moveSnarkWasmPath,
   moveSnarkZkeyPath,
-  BiomebaseSnarkContractCallArgs,
   BiomebaseSnarkInput,
   biomebaseSnarkWasmPath,
   biomebaseSnarkZkeyPath,
-  RevealSnarkContractCallArgs,
   RevealSnarkInput,
   revealSnarkWasmPath,
   revealSnarkZkeyPath,
 } from "@darkforest_eth/snarks";
-
-import { modPBigInt } from "@darkforest_eth/hashing";
-const CACHE_SIZE: number = parseInt(process?.env?.CACHE_SIZE || "10000");
-const InMemoryCache = new LRUMap(CACHE_SIZE);
 
 export async function getMoveArgs(
   x1: number,
@@ -34,18 +25,13 @@ export async function getMoveArgs(
   SCALE: number,
   xMirror: number,
   yMirror: number
-): Promise<MoveSnarkContractCallArgs> {
+): Promise<SnarkJSProofAndSignals> {
   try {
-    const cacheKey = `${x1}-${y1}-${x2}-${y2}-${r}-${distMax}`;
-    const cachedResult = InMemoryCache.get(cacheKey);
-    if (cachedResult) {
-      return cachedResult as MoveSnarkContractCallArgs;
-    }
     const input: MoveSnarkInput = {
-      x1: modPBigInt(x1).toString(),
-      y1: modPBigInt(y1).toString(),
-      x2: modPBigInt(x2).toString(),
-      y2: modPBigInt(y2).toString(),
+      x1: x1.toString(),
+      y1: y1.toString(),
+      x2: x2.toString(),
+      y2: y2.toString(),
       r: r.toString(),
       distMax: distMax.toString(),
       PLANETHASH_KEY: PLANETHASH_KEY.toString(),
@@ -55,21 +41,11 @@ export async function getMoveArgs(
       yMirror: yMirror.toString(),
     };
 
-    const {
-      proof,
-      publicSignals,
-    }: SnarkJSProofAndSignals = await snarkjs.groth16.fullProve(
+    return await snarkjs.groth16.fullProve(
       input,
       moveSnarkWasmPath,
       moveSnarkZkeyPath
     );
-
-    const proofArgs = buildContractCallArgs(
-      proof,
-      publicSignals
-    ) as MoveSnarkContractCallArgs;
-    InMemoryCache.set(cacheKey, proofArgs);
-    return proofArgs;
   } catch (e) {
     throw e;
   }
@@ -83,13 +59,12 @@ export async function getFindArtifactArgs(
   SCALE: number,
   xMirror: number,
   yMirror: number
-): Promise<BiomebaseSnarkContractCallArgs> {
+): Promise<SnarkJSProofAndSignals> {
   try {
     const start = Date.now();
-    console.log("ARTIFACT: calculating witness and proof");
     const input: BiomebaseSnarkInput = {
-      x: modPBigInt(x).toString(),
-      y: modPBigInt(y).toString(),
+      x: x.toString(),
+      y: y.toString(),
       PLANETHASH_KEY: PLANETHASH_KEY.toString(),
       BIOMEBASE_KEY: BIOMEBASE_KEY.toString(),
       SCALE: SCALE.toString(),
@@ -97,18 +72,11 @@ export async function getFindArtifactArgs(
       yMirror: yMirror.toString(),
     };
 
-    const {
-      proof,
-      publicSignals,
-    }: SnarkJSProofAndSignals = await snarkjs.groth16.fullProve(
+    return await snarkjs.groth16.fullProve(
       input,
       biomebaseSnarkWasmPath,
       biomebaseSnarkZkeyPath
     );
-
-    const proofArgs = buildContractCallArgs(proof, publicSignals);
-
-    return proofArgs as BiomebaseSnarkContractCallArgs;
   } catch (e) {
     throw e;
   }
@@ -122,11 +90,11 @@ export async function getRevealArgs(
   SCALE: number,
   xMirror: number,
   yMirror: number
-): Promise<RevealSnarkContractCallArgs> {
+): Promise<SnarkJSProofAndSignals> {
   try {
     const input: RevealSnarkInput = {
-      x: modPBigInt(x).toString(),
-      y: modPBigInt(y).toString(),
+      x: x.toString(),
+      y: y.toString(),
       PLANETHASH_KEY: PLANETHASH_KEY.toString(),
       SPACETYPE_KEY: SPACETYPE_KEY.toString(),
       SCALE: SCALE.toString(),
@@ -134,20 +102,11 @@ export async function getRevealArgs(
       yMirror: yMirror.toString(),
     };
 
-    const {
-      proof,
-      publicSignals,
-    }: SnarkJSProofAndSignals = snarkjs.groth16.fullProve(
+    return snarkjs.groth16.fullProve(
       input,
       revealSnarkWasmPath,
       revealSnarkZkeyPath
     );
-    const ret = buildContractCallArgs(
-      proof,
-      publicSignals
-    ) as RevealSnarkContractCallArgs;
-
-    return ret;
   } catch (e) {
     throw e;
   }
